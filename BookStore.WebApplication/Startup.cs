3972091f999f63acf622a546453f77c8ebad6b15
khuongnguyen0412 <lapankhuongnguyen@gmail.com>
2022-07-04 12:42:42 +0700

    Added identity cookie

--- BookStore.WebApplication/Startup.cs
+++ BookStore.WebApplication/Startup.cs
@@ -26,37 +26,42 @@ namespace BookStore.WebApplication
         public void ConfigureServices(IServiceCollection services)
         {
             ServiceInjection.AddProjectModules(services);
-            services.AddControllersWithViews(o=>o.Filters.Add(new AuthorizeFilter()));
-            services.AddAuthentication("Cookies").AddCookie();
-        }
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        {
-            if (env.IsDevelopment())
+            services.AddControllersWithViews(o => o.Filters.Add(new AuthorizeFilter()));
+            services.AddAuthentication("Cookies").AddCookie(o => o.Events = new Microsoft.AspNetCore.Authentication.Cookies.CookieAuthenticationEvents
             {
-                app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-                app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
-            app.UseHttpsRedirection();
-            app.UseStaticFiles();
-
-            app.UseRouting();
-            app.UseAuthentication();
-
-            app.UseAuthorization();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllerRoute(
-                    name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
-            });
-        }
+                OnValidatePrincipal = async (c) =>
+                {
+                    c.RejectPrincipal();
+                });
     }
+
+    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        if (env.IsDevelopment())
+        {
+            app.UseDeveloperExceptionPage();
+        }
+        else
+        {
+            app.UseExceptionHandler("/Home/Error");
+            // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+            app.UseHsts();
+        }
+        app.UseHttpsRedirection();
+        app.UseStaticFiles();
+
+        app.UseRouting();
+        app.UseAuthentication();
+
+        app.UseAuthorization();
+
+        app.UseEndpoints(endpoints =>
+        {
+            endpoints.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+        });
+    }
+}
 }
